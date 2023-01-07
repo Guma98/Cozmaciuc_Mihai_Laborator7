49f71de2d0e7af3f33ee27c0e33853d966eb53c2
Guma98 <cozmaciuc_mihai@yahoo.com>
2023-01-07 20:28:29 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Cozmaciuc_Mihai_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cozmaciuc_Mihai_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
